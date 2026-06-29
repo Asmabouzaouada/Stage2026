0439de67f2f6ff0497fa648afd4eef54e6d06e29
Asmabouzaouada <abouzaouada@gmail.com>
2026-06-29 14:19:09 +0100

    Configuration API URL par environnement pour Kubernetes

--- src/main/java/tn/esprit/smartrhback/Security/SecurityConfig.java
+++ src/main/java/tn/esprit/smartrhback/Security/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:*", "http://192.168.*.*:*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
